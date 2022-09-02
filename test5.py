c3107bea8ccf9ca5e0e0c698eee4dd15b9418b0f
관리자 <gwanlija@joyunaui-MacBookAir.local>
2022-09-03 00:48:27 +0900

    튜플ans.py

--- test5.py
+++ test5.py
@@ -1,17 +1,4 @@
-def solution(phone_book):
-    answer = True
+from collections import Counter
 
-    phone_book.sort()
-    
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            return False
-            
-    return answer
-
-phone_book = ["119", "97674223", "1195524421"]
-
-print(solution(phone_book))
-
-print("11901".startswith("119"))
-print("911901".startswith("119"))
+counter = Counter("hello world")
+print(counter["o"])
